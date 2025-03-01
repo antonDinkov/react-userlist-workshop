b3fd35e1acd60121fb2c788b9489f54486368644
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-03-02 00:01:57 +0200

    Add button modal to the middle

---
+++ client/src/components/AddNewUser.jsx
@@ -0,0 +1,9 @@
+function AddNewUser(props) {
+    return (
+        <>
+            <button className="btn-add btn" onClick={props.onClick}>Add new user</button>{/* 229 ред в App.jsx */}
+        </>
+    )
+}
+
+export default AddNewUser;
